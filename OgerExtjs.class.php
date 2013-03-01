f21fbab79a84789b263e99fc857f7069d387c905
gerhard <gerhard.oettl@ogersoft.at>
2013-03-01 16:06:35 +0100

    bugfix extjs

--- OgerExtjs.class.php
+++ OgerExtjs.class.php
@@ -12,41 +12,41 @@ class OgerExtjs {
 
 
   /**
-  * Encode an object from a php array into json.
-  * @param $array The array with the object values.
+  * Encode a php array into json.
+  * @param $array The array with values.
   * @param $success Boolean flag for the success property. Defaults to true.<br>
   *        - Null: Do not set a new and do not change an existing success property.
-  * @return Json encoded object.
+  * @return Json encoded array.
   */
-  public static function enc($obj = array(), $success = true) {
+  public static function enc($array = array(), $success = true) {
     if ($success !== null) {
-      $obj["success"] = (boolean)$success;
+      $array["success"] = (boolean)$success;
     }
-    return json_encode($obj);
-  }  // eo json encoded object
+    return json_encode($array);
+  }  // eo json encoded array
 
 
   /**
   * Encode data from a php array into json.
-  * @param $obj The array with the data values.
+  * @param $array The array with the data values.
   * @param $root Name of the data root property. Defaults to "data".
-  * @return Json encoded object.
+  * @return Json encoded array.
   */
-  public static function encData($obj = array(), $root = null, $more = array()) {
+  public static function encData($array = array(), $root = null, $more = array()) {
     if (!$root) {
       $root = "data";
     }
     $all = $more;
-    $all[$root] = $obj;
-    // data objects always are success respones
-    return static::enc($data, true);
-  }  // eo json encoded data object
+    $all[$root] = $array;
+    // data array always are success respones
+    return static::enc($all, true);
+  }  // eo json encoded data array
 
 
   /**
   * Encode an error message.
   * @param $msg The error message.
-  * @return Json encoded object.
+  * @return Json encoded array.
   */
   public static function errorMsg($msg) {
     return static::enc(array("msg" => $msg), false);
