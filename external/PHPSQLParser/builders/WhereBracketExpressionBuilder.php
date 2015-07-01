55c799a0bf0d7981bad9aaefd131d32a8b633e9d
gerhard <gerhard.oettl@ogersoft.at>
2015-07-01 16:11:04 +0200

    sync php sql parser with upstream

--- external/PHPSQLParser/builders/WhereBracketExpressionBuilder.php
+++ external/PHPSQLParser/builders/WhereBracketExpressionBuilder.php
@@ -31,12 +31,12 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * 
  * @author    André Rothe <andre.rothe@phosco.info>
  * @copyright 2010-2014 Justin Swanhart and André Rothe
  * @license   http://www.debian.org/misc/bsd.license  BSD License (3 Clause)
  * @version   SVN: $Id$
- *
+ * 
  */
 
 namespace PHPSQLParser\builders;
@@ -44,12 +44,12 @@ use PHPSQLParser\exceptions\UnableToCreateSQLException;
 use PHPSQLParser\utils\ExpressionType;
 
 /**
- * This class implements the builder for bracket expressions within the WHERE part.
+ * This class implements the builder for bracket expressions within the WHERE part. 
  * You can overwrite all functions to achieve another handling.
  *
  * @author  André Rothe <andre.rothe@phosco.info>
  * @license http://www.debian.org/misc/bsd.license  BSD License (3 Clause)
- *
+ *  
  */
 class WhereBracketExpressionBuilder implements Builder {
 
