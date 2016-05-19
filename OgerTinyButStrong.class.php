ea2e5fc700989fc7db7e124990fb17fda324d4fe
oger000 <gerhard.oettl@ogersoft.at>
2016-05-19 08:40:36 +0200

    include tiny but strong lib into oger php libs

---
+++ OgerTinyButStrong.class.php
@@ -0,0 +1,46 @@
+<?PHP
+/*
+#LICENSE BEGIN
+#LICENSE END
+*/
+
+
+
+
+/**
+* Wrapper for tinybutstrong lib to fit
+* class detection and file including of init.php
+* www.tinybutstrong.com
+*/
+
+
+
+// load original class file and used plugins
+require_once(__DIR__ . "/external/tinybutstrong/tbs_class.php");
+require_once(__DIR__ . "/external/tinybutstrong/plugins/tbs_plugin_opentbs.php");
+
+
+/*
+ * Tiny but strong wrapper class
+ */
+class OgerTinyButStrong extends clsTinyButStrong {
+
+
+	/**
+	* Constructor.
+	*/
+	public function __construct($opts = null) {
+
+		// default to restrictive security settings
+		if ($opts === null) {
+			$opts = array("var_prefix" => "otbs_var_", "fct_prefix" => "otbs_vct_");
+		}
+
+		parent::__construct($opts);
+	}  // eo constructor
+
+
+
+}  // eo class
+
+?>
