57788f4996e8d3e6a734b3e563bb4af2ada50992
oger000 <gerhard.oettl@ogersoft.at>
2018-03-23 11:11:18 +0100

    license

--- OgerPdf0.class.php
+++ OgerPdf0.class.php
@@ -1,6 +1,10 @@
 <?PHP
 /*
 #LICENSE BEGIN
+**********************************************************************
+* OgerArch - Archaeological Database is released under the GNU General Public License (GPL) <http://www.gnu.org/licenses>
+* Copyright (C) Gerhard Öttl <gerhard.oettl@ogersoft.at>
+**********************************************************************
 #LICENSE END
 */
 
